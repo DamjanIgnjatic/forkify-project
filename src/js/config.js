94b7ecb714c4adea0931e41d0bb90f544265de86
DamjanIgnjatic <ignjatic.dev@gmail.com>
2023-04-24 20:37:51 +0200

    Inital coomit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6cd11ae5-f0eb-4f11-85ac-b12778a3ef91';
+export const MODAL_CLOSE_SEC = 2.5;
